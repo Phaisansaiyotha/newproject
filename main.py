f758fef50a9e5f2d0093c7b914c298112519306e
Phaisansaiyotha <phaisansaiyotha88@gmail.com>
2023-07-29 07:22:28 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+print("------------")
+
+print("----No Limited Beer Shop----")
+
+print("-----------------")
+
+print("Double IPA x 1 : 30 Baht")
+
+print("Vanilla Milk Stout x 1 : 20 Baht")
+
+print("Single Hop Pale Ale x 1 : 15 Baht")
+
+print("Hazy Cloud IPA x 1 : 20 Baht")
+
+print("Strawberry Saison x 1 : 10 Baht")
+
+print("Banana Weizen x 1 : 15 Baht")
+
+print("Die Hard West Coast IPA x 1 : 15 Baht")
+
+print("Lemon Sour Beer x 1 : 15 Baht")
+
+print("-----------------")
+
+print("Total      : 120 Baht")
